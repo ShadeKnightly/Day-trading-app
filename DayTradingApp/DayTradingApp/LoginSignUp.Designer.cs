d25d7ea7d67cc20ec5d8c3f486f7f6b7ab0a6d8d
ShadeKnightly <147964491+ShadeKnightly@users.noreply.github.com>
2025-12-12 20:34:58 -0700

    fix login centering

--- DayTradingApp/DayTradingApp/LoginSignUp.Designer.cs
+++ DayTradingApp/DayTradingApp/LoginSignUp.Designer.cs
@@ -24,39 +24,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            login_signup_toggle = new System.Windows.Forms.Button();
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             nameInput_Box = new System.Windows.Forms.TextBox();
             emailInput_Box = new System.Windows.Forms.TextBox();
             PassInput_Box = new System.Windows.Forms.TextBox();
             SubmitBtn = new System.Windows.Forms.Button();
-            pictureBox1 = new System.Windows.Forms.PictureBox();
-            tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            login_signup_toggle = new System.Windows.Forms.Button();
+            tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             tableLayoutPanel1.SuspendLayout();
             tableLayoutPanel2.SuspendLayout();
+            tableLayoutPanel3.SuspendLayout();
             SuspendLayout();
             // 
-            // login_signup_toggle
+            // pictureBox1
             // 
-            login_signup_toggle.Anchor = System.Windows.Forms.AnchorStyles.None;
-            login_signup_toggle.BackColor = System.Drawing.Color.Transparent;
-            login_signup_toggle.BackgroundImage = Properties.Resources.toggleLeft;
-            login_signup_toggle.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            login_signup_toggle.FlatAppearance.BorderSize = 0;
-            login_signup_toggle.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            login_signup_toggle.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            login_signup_toggle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            login_signup_toggle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
-            login_signup_toggle.ForeColor = System.Drawing.Color.WhiteSmoke;
-            login_signup_toggle.Location = new System.Drawing.Point(2, 113);
-            login_signup_toggle.Margin = new System.Windows.Forms.Padding(2);
-            login_signup_toggle.Name = "login_signup_toggle";
-            login_signup_toggle.Size = new System.Drawing.Size(253, 33);
-            login_signup_toggle.TabIndex = 0;
-            login_signup_toggle.Text = "LOG IN             SIGN UP";
-            login_signup_toggle.UseVisualStyleBackColor = false;
-            login_signup_toggle.Click += login_signup_toggle_Click;
+            pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            pictureBox1.Image = Properties.Resources.NestLogo;
+            pictureBox1.Location = new System.Drawing.Point(132, 30);
+            pictureBox1.Margin = new System.Windows.Forms.Padding(2, 30, 2, 15);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(211, 170);
+            pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            tableLayoutPanel1.BackgroundImage = Properties.Resources.square;
+            tableLayoutPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 1);
+            tableLayoutPanel1.Controls.Add(pictureBox1, 0, 0);
+            tableLayoutPanel1.Location = new System.Drawing.Point(242, 124);
+            tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47.74096F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 52.25904F));
+            tableLayoutPanel1.Size = new System.Drawing.Size(475, 452);
+            tableLayoutPanel1.TabIndex = 3;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            tableLayoutPanel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            tableLayoutPanel2.ColumnCount = 1;
+            tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            tableLayoutPanel2.Controls.Add(nameInput_Box, 0, 0);
+            tableLayoutPanel2.Controls.Add(emailInput_Box, 0, 1);
+            tableLayoutPanel2.Controls.Add(PassInput_Box, 0, 2);
+            tableLayoutPanel2.Controls.Add(SubmitBtn, 0, 4);
+            tableLayoutPanel2.Controls.Add(login_signup_toggle, 0, 3);
+            tableLayoutPanel2.Location = new System.Drawing.Point(109, 217);
+            tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 45);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 5;
+            tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            tableLayoutPanel2.Size = new System.Drawing.Size(257, 189);
+            tableLayoutPanel2.TabIndex = 0;
             // 
             // nameInput_Box
             // 
@@ -118,89 +154,73 @@
             SubmitBtn.UseVisualStyleBackColor = true;
             SubmitBtn.Click += SubmitBtn_Click;
             // 
-            // pictureBox1
+            // login_signup_toggle
             // 
-            pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            pictureBox1.Image = Properties.Resources.NestLogo;
-            pictureBox1.Location = new System.Drawing.Point(210, 30);
-            pictureBox1.Margin = new System.Windows.Forms.Padding(2, 30, 2, 15);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new System.Drawing.Size(211, 170);
-            pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            pictureBox1.TabIndex = 3;
-            pictureBox1.TabStop = false;
+            login_signup_toggle.Anchor = System.Windows.Forms.AnchorStyles.None;
+            login_signup_toggle.BackColor = System.Drawing.Color.Transparent;
+            login_signup_toggle.BackgroundImage = Properties.Resources.toggleLeft;
+            login_signup_toggle.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            login_signup_toggle.FlatAppearance.BorderSize = 0;
+            login_signup_toggle.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            login_signup_toggle.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            login_signup_toggle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            login_signup_toggle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            login_signup_toggle.ForeColor = System.Drawing.Color.WhiteSmoke;
+            login_signup_toggle.Location = new System.Drawing.Point(2, 113);
+            login_signup_toggle.Margin = new System.Windows.Forms.Padding(2);
+            login_signup_toggle.Name = "login_signup_toggle";
+            login_signup_toggle.Size = new System.Drawing.Size(253, 33);
+            login_signup_toggle.TabIndex = 0;
+            login_signup_toggle.Text = "LOG IN             SIGN UP";
+            login_signup_toggle.UseVisualStyleBackColor = false;
+            login_signup_toggle.Click += login_signup_toggle_Click;
             // 
-            // tableLayoutPanel1
+            // tableLayoutPanel3
             // 
-            tableLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
-            tableLayoutPanel1.BackgroundImage = Properties.Resources.square;
-            tableLayoutPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            tableLayoutPanel1.ColumnCount = 1;
-            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 1);
-            tableLayoutPanel1.Controls.Add(pictureBox1, 0, 0);
-            tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
-            tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.RowCount = 2;
-            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47.74096F));
-            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 52.25904F));
-            tableLayoutPanel1.Size = new System.Drawing.Size(631, 451);
-            tableLayoutPanel1.TabIndex = 3;
-            // 
-            // tableLayoutPanel2
-            // 
-            tableLayoutPanel2.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            tableLayoutPanel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            tableLayoutPanel2.ColumnCount = 1;
-            tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            tableLayoutPanel2.Controls.Add(nameInput_Box, 0, 0);
-            tableLayoutPanel2.Controls.Add(emailInput_Box, 0, 1);
-            tableLayoutPanel2.Controls.Add(PassInput_Box, 0, 2);
-            tableLayoutPanel2.Controls.Add(SubmitBtn, 0, 4);
-            tableLayoutPanel2.Controls.Add(login_signup_toggle, 0, 3);
-            tableLayoutPanel2.Location = new System.Drawing.Point(187, 217);
-            tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 45);
-            tableLayoutPanel2.Name = "tableLayoutPanel2";
-            tableLayoutPanel2.RowCount = 5;
-            tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            tableLayoutPanel2.Size = new System.Drawing.Size(257, 189);
-            tableLayoutPanel2.TabIndex = 0;
+            tableLayoutPanel3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            tableLayoutPanel3.ColumnCount = 3;
+            tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            tableLayoutPanel3.Controls.Add(tableLayoutPanel1, 1, 1);
+            tableLayoutPanel3.Location = new System.Drawing.Point(0, 0);
+            tableLayoutPanel3.Name = "tableLayoutPanel3";
+            tableLayoutPanel3.RowCount = 3;
+            tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            tableLayoutPanel3.Size = new System.Drawing.Size(959, 700);
+            tableLayoutPanel3.TabIndex = 4;
             // 
             // LoginSignUp
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            Controls.Add(tableLayoutPanel1);
+            Controls.Add(tableLayoutPanel3);
             DoubleBuffered = true;
             Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             Name = "LoginSignUp";
-            Size = new System.Drawing.Size(631, 451);
+            Size = new System.Drawing.Size(959, 700);
             Load += LoginSignUp_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             tableLayoutPanel1.ResumeLayout(false);
             tableLayoutPanel2.ResumeLayout(false);
             tableLayoutPanel2.PerformLayout();
+            tableLayoutPanel3.ResumeLayout(false);
             ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button login_signup_toggle;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
         private System.Windows.Forms.TextBox nameInput_Box;
         private System.Windows.Forms.TextBox emailInput_Box;
         private System.Windows.Forms.TextBox PassInput_Box;
         private System.Windows.Forms.Button SubmitBtn;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button login_signup_toggle;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
     }
 }
